3ca3df9d3c88045849897f7fc223e5cfae56e1c7
Simon Landry <landry_simon@pm.me>
2018-06-20 18:19:37 -0400

    Fix Database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,19 @@
-b = File.open('db/sudokus.csv', 'r').readlines.map { |x| x.split(',').map(&:chomp).map(&:to_i) }.map do |x|
-  position = -1
-  b = Board.new
-  b.save(validate: false)
-  x.map do |y|
-    position += 1
-    if y.nonzero?
-      { board: b.id, content: y, position: position, set: true }
-    else
-      { board: b.id, content: nil, position: position, set: false }
+# Boards and Cells seeds
+cells =
+  File
+  .open('db/sudokus.csv', 'r')
+  .readlines
+  .map { |x| x.split(',').map(&:chomp).map(&:to_i) }
+  .map do |row|
+    position = -1
+    b = Board.new
+    b.save(validate: false)
+    row.map do |digit|
+      position += 1
+      { board: b.id, content: digit.zero? ? nil : digit, position: position }
     end
   end
-end
 
-values = b.flatten.map { |e| "(#{e.map { |_k, v| v.nil? ? 'null' : !!v == v ? v ? 1 : 0 : v }.join(',')})" }.join(',')
-ActiveRecord::Base.connection.execute("INSERT INTO cells (\"board_id\", \"content\", \"position\", \"set\") VALUES #{values}")
+# Here we use Raw SQL to skip validations for faster inserts
+values = cells.flatten.map { |e| "(#{e.map { |_k, v| v.nil? ? 'null' : !!v == v ? v ? 1 : 0 : v }.join(',')})" }.join(',')
+ActiveRecord::Base.connection.execute("INSERT INTO cells (\"board_id\", \"content\", \"position\") VALUES #{values}")
