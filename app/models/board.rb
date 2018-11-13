55877cfc9bd79717a59640966418a0d9200d364e
Simon Landry <landry_simon@pm.me>
2018-11-13 00:26:13 -0500

    Rubocop autocorrect

--- app/models/board.rb
+++ app/models/board.rb
@@ -9,13 +9,13 @@ class Board < ApplicationRecord
     b.save(validate: false)
     cells =
       board_string
-        .split(',')
-        .map(&:chomp)
-        .map(&:to_i)
-        .map do |digit|
-          position += 1
-          { board: b.id, content: digit.zero? ? nil : digit, position: position }
-        end
+      .split(',')
+      .map(&:chomp)
+      .map(&:to_i)
+      .map do |digit|
+        position += 1
+        { board: b.id, content: digit.zero? ? nil : digit, position: position }
+      end
 
     values = cells.flatten.map { |e| "(#{e.map { |_k, v| v.nil? ? 'null' : !!v == v ? v ? 1 : 0 : v }.join(',')})" }.join(',')
     ActiveRecord::Base.connection.execute("INSERT INTO cells (\"board_id\", \"content\", \"position\") VALUES #{values}")
@@ -35,19 +35,19 @@ class Board < ApplicationRecord
       ORDER BY RANDOM()
       LIMIT 1
     ").first ||
-      Board.find_by_sql("
+      Board.find_by_sql('
         SELECT *
         FROM boards
         ORDER BY RANDOM()
         LIMIT 1
-      ").first
+      ').first
   end
 
   def get_scores
     Game.where(board: self)
-      .map { |x| { user: x.user.name, time_spent: x.finished_at - x.created_at} }
-      .sort_by { |x| x[:time_spent] }
-      .uniq { |x| x[:user] }
+        .map { |x| { user: x.user.name, time_spent: x.finished_at - x.created_at } }
+        .sort_by { |x| x[:time_spent] }
+        .uniq { |x| x[:user] }
   end
 
   def as_json(_options = {})
